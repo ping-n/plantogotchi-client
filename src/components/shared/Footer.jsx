9abbc41a958f2173b548f73d0f8352c55556e1fa
ping-n <59724381+ping-n@users.noreply.github.com>
2020-07-17 16:38:08 +1000

    added Footer component

---
+++ src/components/shared/Footer.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <Link to="/About">ABOUT</Link>
+                <Link to="/Faq">FAQ</Link>
+                <a href="https://github.com/ping-n/travel-diary-client">
+                    @2020
+                </a>
+            </footer>
+        )
+    }
+}
+
+export default Footer
