e83d9b1d2338787f7745a84d86ae175d67527ca3
nova <alexander.coole@protonmail.com>
2020-07-17 12:51:10 +1000

    implemented very basic login, no error handling

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
-import { plants } from '../classes/PlantApi'
-import { users } from '../classes/UserApi';
-import { breeds } from '../classes/BreedApi'
+import React from "react";
+import { plants } from "../classes/PlantApi";
+import { users } from "../classes/UserApi";
+import { breeds } from "../classes/BreedApi";
 
 class Test extends React.Component {
-    render() {
-        plants.index().then((response) => {
-            this.setState({plants: response.plants})
-        })
-        users.index().then((response) => {
-            this.setState({users: response.users})
-        })
-        breeds.index().then((response) => {
-            this.setState({breeds: response.breeds})
-        })
-            
-        return (<h1>hey</h1>)
-    }
+  render() {
+    plants
+      .index()
+      .then((response) => {
+        this.setState({ plants: response.plants });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    users
+      .index()
+      .then((response) => {
+        this.setState({ users: response.users });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    breeds
+      .index()
+      .then((response) => {
+        this.setState({ breeds: response.breeds });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    return <h1>hey</h1>;
+  }
 }
 
 export default Test;
