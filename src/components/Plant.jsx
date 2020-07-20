47604465c90728b0c57ebd8d8022f08df3505a26
nova <alexander.coole@protonmail.com>
2020-07-20 17:20:21 +1000

    changed position of state/update methods within in plant hierarchy

--- src/components/Plant.jsx
+++ src/components/Plant.jsx
@@ -1,24 +1,36 @@
 import React from "react";
 import { plants } from "../classes/PlantApi";
-import { breeds } from "../classes/BreedApi"
+import { breeds } from "../classes/BreedApi";
 import { Card, Image } from "semantic-ui-react";
 import WaterLevel from "./Plant/WaterLevel";
 
 export default class Plant extends React.Component {
-  state = { plant: {}, breed_name: '' };
+  state = { plant: this.props.location.plant.plant, breed_name: "" };
 
   componentDidMount() {
-    plants
-      .show(this.props.match.params.id)
-      .then((res) => this.setState({ plant: res.data }))
-      .catch((er) => console.log(er));
+    console.log(this.props.location.plant);
+    if (!this.state.plant) {
+      plants
+        .show(this.props.match.params.id)
+        .then((res) => this.setState({ plant: res.data }))
+        .catch((er) => console.log(er));
+    }
     breeds
       .show(this.state.plant.breed_id)
       .then((res) => {
-        console.log(res)
-        this.setState({ breed_name: res.data.name })}
-        )
+        console.log(res);
+        this.setState({ breed_name: res.data.name });
+      })
       .catch((er) => console.log(er));
+  }
+
+  updateWaterLevel(id, level) {
+    const params = {
+      plant: {
+        water_level: level,
+      },
+    };
+    plants.update(id, params);
   }
 
   render() {
@@ -32,8 +44,7 @@ export default class Plant extends React.Component {
       growth_stage,
       created_at,
     } = this.state.plant;
-    const breed_name = this.state.breed_name
-
+    const breed_name = this.state.breed_name;
 
     return (
       <Card>
@@ -45,7 +56,11 @@ export default class Plant extends React.Component {
           </Card.Meta>
           <Card.Description>
             {/* {name} is of the {breed_name} breed. */}
-            <WaterLevel id={id} level={100} />
+            <WaterLevel
+              id={id}
+              level={water_level}
+              updateWaterLevel={this.updateWaterLevel}
+            />
             <h3>food level: {food_level}</h3>
             <h3>growth stage: {growth_stage}</h3>
             {alive && <h3>they are alive!</h3>}
--- src/components/Plants.jsx
+++ src/components/Plants.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { plants } from "../classes/PlantApi";
 import PlantCard from "./PlantCard";
 import { Grid } from "semantic-ui-react";
@@ -10,8 +10,8 @@ export default class Plants extends React.Component {
     plants
       .index()
       .then((res) => {
-        console.log(res)
-        this.setState({ plants: res.data })
+        console.log(res);
+        this.setState({ plants: res.data });
       })
       .catch((er) => console.log(er));
   }
@@ -20,17 +20,7 @@ export default class Plants extends React.Component {
     const plantsArr = this.state.plants.map((plant, index) => {
       return (
         <Grid.Column>
-          <PlantCard
-            key={index}
-            id={plant.id}
-            alive={plant.alive}
-            name={plant.name}
-            water_level={plant.water_level}
-            food_level={plant.food_level}
-            growth_stage={plant.growth_stage}
-            breed_name={plant.breed.name}
-            created_at={plant.created_at}
-          />
+          <PlantCard key={index} plant={plant} />
         </Grid.Column>
       );
     });
