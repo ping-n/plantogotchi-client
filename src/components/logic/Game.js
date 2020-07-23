97c9732ccad672ea1ba0b785b696aea8f304f1d4
PING NGE <ping.nge@hotmail.com>
2020-07-23 18:06:50 +1000

    game logic functioning and water level working

--- src/components/logic/Game.js
+++ src/components/logic/Game.js
@@ -1,5 +1,5 @@
 class Game {
-  constructor(growth_speed = 1, water_drop_speed = 1, game_speed = 1000) {
+  constructor(growth_speed = 1, water_drop_speed = 1, game_speed = 2000) {
     this.growth_speed = growth_speed;
     this.water_drop_speed = water_drop_speed;
     this.game_speed = game_speed;
