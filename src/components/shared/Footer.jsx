2726f6a35e33587b510d016d477fb984dcf8ed1f
PING NGE <ping.nge@hotmail.com>
2020-07-25 16:12:45 +1000

    add the correct position for links for nav and footer

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -4,16 +4,20 @@ import { Link } from "react-router-dom";
 class Footer extends Component {
   render() {
     return (
-      <footer>
-        <Link to="/About">ABOUT</Link>
-        <Link to="/Faq">FAQ</Link>
-        <a
-          href="https://github.com/ping-n/plantogotchi-client"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          @2020
-        </a>
+      <footer className="footer">
+        <div className="footer-controls">
+          <Link to="/About">ABOUT</Link>
+          <Link to="/Faq">FAQ</Link>
+        </div>
+        <div className="footer-cr">
+          <a
+            href="https://githb.com/ping-n/plantogotchi-client"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @2020
+          </a>
+        </div>
       </footer>
     );
   }
--- src/components/shared/LoggedIn.jsx
+++ src/components/shared/LoggedIn.jsx
@@ -1,38 +1,39 @@
 import React from "react";
-import { Link, Redirect } from "react-router-dom"
-import { Button } from "semantic-ui-react"
+import { Link, Redirect } from "react-router-dom";
+import { Button } from "semantic-ui-react";
 
 const handleLogOut = (props) => {
-  localStorage.removeItem("token")
-  localStorage.removeItem("auth")
-  props.history.push("/")
-  alert("You have logged out successfully.")
-  return <Redirect to="/" />
-}
+  localStorage.removeItem("token");
+  localStorage.removeItem("auth");
+  props.history.push("/");
+  alert("You have logged out successfully.");
+  return <Redirect to="/" />;
+};
 
 const LoggedIn = (props) => {
-
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link data-testid="myaccount" to="/myaccount">
-        My Account
-      </Link>
-      <Link data-testid="plants" to="/plants">
-        Plants
-      </Link>
-      <Link data-testid="createbreed" to="/createbreed">
-        Create Breed
-      </Link>
-      <Link data-testid="createplant" to="/createplant">
-        Create Plant
-      </Link>
-      <Link data-testid="breeds" to="/breeds">
-        Breeds
-      </Link>
-      <Button onClick={() => handleLogOut(props)}>
-        Log Out
-      </Button>
+    <div className="nav">
+      <div className="nav-home">
+        <Link to="/">Home</Link>
+      </div>
+      <div className="nav-controls">
+        <Link data-testid="myaccount" to="/myaccount">
+          My Account
+        </Link>
+        <Link data-testid="plants" to="/plants">
+          Plants
+        </Link>
+        <Link data-testid="createbreed" to="/createbreed">
+          Create Breed
+        </Link>
+        <Link data-testid="createplant" to="/createplant">
+          Create Plant
+        </Link>
+        <Link data-testid="breeds" to="/breeds">
+          Breeds
+        </Link>
+        <Button onClick={() => handleLogOut(props)}>Log Out</Button>
+      </div>
     </div>
   );
 };
