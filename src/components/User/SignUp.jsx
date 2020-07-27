5fa64bc609a4aa8da83d6a272d5d7e5f25fcd530
PING NGE <ping.nge@hotmail.com>
2020-07-27 13:40:09 +1000

    changed postion of form to top instead of centre

--- src/components/User/SignUp.jsx
+++ src/components/User/SignUp.jsx
@@ -34,7 +34,7 @@ export class SignUp extends Component {
       <Grid
         textAlign="center"
         style={{ height: "100vh" }}
-        verticalAlign="middle"
+        verticalAlign="top"
       >
         <Grid.Column style={{ maxWidth: 500 }}>
           <Header as="h1" color="black">
