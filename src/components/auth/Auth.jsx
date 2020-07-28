b108caf3f40652ebdc585dbb35cf2448fb0de4f0
nova <alexander.coole@protonmail.com>
2020-07-28 14:27:31 +1000

    added admin logic to auth

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -3,6 +3,7 @@ import { users } from "../../classes/UserApi";
 class Auth {
   constructor() {
     this.authenticated = false;
+    this.admin = false;
   }
 
   login(cb) {
@@ -12,25 +13,35 @@ class Auth {
 
   logout(cb) {
     this.authenticated = false;
+    this.admin = false;
     cb();
   }
 
   async isAuthenticated() {
     await this.hasToken();
-    console.log(`authenticated: ${this.authenticated}`);
     return this.authenticated;
+  }
+
+  async isAdmin() {
+    await this.hasToken();
+    return this.admin;
   }
 
   async hasToken() {
     await users
       .status()
       .then((response) => {
-        if (response.status >= 400) {
+        if (response.status === 401) {
           throw new Error("Not Authorized");
+        } else if (response.status >= 400) {
+          throw new Error("Server Error");
         } else {
-          const { jwt } = response.data;
+          const { jwt, admin } = response.data;
           localStorage.setItem("token", jwt);
           this.authenticated = true;
+          if (admin) {
+            this.admin = true;
+          }
         }
       })
       .catch((error) => {
